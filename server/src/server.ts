3f287ce73b7c43d4ce4192791d0db4f852d5903e
Vlad Bolkunov <gloryson@mail.ru>
2023-04-04 07:47:32 +0200

    feat: added mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-
+import { MongoClient } from 'mongodb';
 
 
 const app = express();
 const port = process.env.PORT || 3001;
+const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/');
+
 
 app.use(cors()).listen(port, () => {
   console.log(`Application started on port ${port}!`);
@@ -13,6 +14,44 @@ app.use(cors()).listen(port, () => {
 
 
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Application started!`);
-});
+async function connect () {
+
+  app.get('/db/:string', async (req: Request, res: Response) => {
+
+    try {
+      let db = mongoClient.db('fullstack-typescript');
+		  let collection = db.collection('todo-list');
+
+      switch (req.params.string) {
+
+        case 'add': {
+          await collection.insertOne({text: 'Lorem', completed: true});
+          res.redirect('/db/show');
+          break;
+        }
+
+        case 'del': {
+          await collection.deleteOne({text: 'Lorem'});
+          res.redirect('/db/show');
+          break;
+        }
+
+        case 'show': {
+          let goods = await collection.find().toArray();
+          res.send(goods);
+          break;
+        }
+
+        default: res.send('Idiot');
+
+      }
+      
+    } catch (error) {
+        res.send(error);
+    }
+
+  })
+
+}
+
+connect();
